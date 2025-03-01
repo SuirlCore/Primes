2d7841092c72472ebf8f676979dbb133a2468043
SuirlCore <152679258+SuirlCore@users.noreply.github.com>
2025-02-28 21:27:09 -0700

    update

--- multiPrimesTest.py
+++ multiPrimesTest.py
@@ -5,35 +5,11 @@ from datetime import datetime       #used to find current time for tracking how 
 import threading                    #used to create multiple threads for event based programming.
 import keyboard                     #detects if a key is pressed
 
-#database connection information. hostname, username, password, database name.
-#gather login information for the database
-print("Enter location of the database to be used:")
-print("1 - Localhost from BadPenny")
-print("2 - Odin @ 192.168.1.39 (On HufflepuffCommonroom wifi)")
-print("3 - Odin @ ? (On Speed-Racer wifi)")
-print("4 - Bad Penny 192.168.1.74 (On HufflepuffCommonroom wifi)")
-print("5 - Other location")
-databaseLocation = input()
-if databaseLocation == "1":
-    databaseHost = ["127.0.0.1", "root", "letmeinnow", "primes"]
-elif databaseLocation == "2":
-    databaseHost = ["192.168.1.39", "suirl", "letmeinnow", "primes"]
-elif databaseLocation == "3":
-    databaseHost = ["?", "suirl", "letmeinnow", "primes"]
-elif databaseLocation == "4":
-    databaseHost = ["192.168.1.74", "root", "letmeinnow", "primes"]
-elif databaseLocation == "5":
-    print("Please enter the IP address for the database")
-    databaseIPAddress = input()
-    print("What is the username for the database?")
-    databaseUsername = input()
-    print("What is the password for the database?")
-    databasePassword = input()
-    databaseHost = [databaseIPAddress, databaseUsername, databasePassword, "primes"]
+#set the database variables
+databaseHost = ["127.0.0.1", "suirl", "letmeinnow", "primes"]
 
-#grab the computers username from the user
-print("What name would you like this instance to use?")
-userNameInput = input()
+#grab the computers hostname
+userNameInput = socket.gethostname()
 
 # -----------------------------
 # functions for multi threading
@@ -62,17 +38,15 @@ def calculating():
             if divisors <= 1:
                 print("New prime found: " + str(newTest))
                 multiSavePrime(newTest)
-            
-        #ends this task if the userInput task is not still running
-        if inputTask.is_alive() == False:
-            break
 
-#look for the q key to be pressed, and end the task
-def userInput():
-    print("Start looking for input\n")
-    while True:
-        if keyboard.is_pressed('q'):
-            break
+            #slow things down a bit. pauses by 1/10th of a second after each calculation
+            time.sleep(.1)
+
+        #edit the database to show the range has been completed
+        sqlInput = "UPDATE inProgress SET inProgress = '0' WHERE userID = '" + userNameInput + "';"
+        multiUpdate(sqlInput)
+
+
 
 
 # -------------------------------------------------------------------------------------
@@ -135,72 +109,85 @@ def multiUpdate(sqlInput):
 # - inProgress: instanceID (PRI, int, auto increment), userID (varchar), numStartChecking (int), numEndChecking (int)
 def multiLoadRange():
     print("Pulling the next range from the database\n")
-    #if there is a range in inProgress table, grab that and add 1 and 100
-    sqlInput = "SELECT MAX(numEndChecking) as numStart FROM inProgress;"
-    lastRange = multiSelect(sqlInput)
-    for x in lastRange:		#lastRange is a list of tuples. iterates through to grab an int.
-        for y in x:
-            lastRangeInt = y
-
-    #if no range in inProgress, grab last prime found and add 1 and 100
-    if lastRangeInt == None:
-        sqlInput = "SELECT MAX(multiPrimeNum) as lastPrime FROM multiPrimes;"
+    userName = userNameInput
+    
+    # Check if there is an existing inProgress range for the current hostname
+    sqlInput = f"""
+        SELECT numStartChecking, numEndChecking FROM inProgress 
+        WHERE userID = '{userName}' AND inProgress = 1 
+        LIMIT 1;
+    """
+    existingRange = multiSelect(sqlInput)
+    
+    if existingRange:
+        currentRangeStart, currentRangeEnd = existingRange[0]
+        print(f"Using existing range: {currentRangeStart} - {currentRangeEnd}")
+        
+        # Delete existing primes in the selected range
+        sqlInput = f"""
+            DELETE FROM multiPrimes WHERE multiPrimeNum BETWEEN {currentRangeStart} AND {currentRangeEnd};
+        """
+        multiUpdate(sqlInput)
+    else:
+        # If no existing range, find the last checked range
+        sqlInput = "SELECT MAX(numEndChecking) as numStart FROM inProgress;"
         lastRange = multiSelect(sqlInput)
-        for x in lastRange:		#lastPrime is a list of tuples. iterates through to grab an int.
-            for y in x:
-                lastRangeInt = y
+        lastRangeInt = next((y for x in lastRange for y in x), None)
+        
+        # If no range found, use the last prime found
+        if lastRangeInt is None:
+            sqlInput = "SELECT MAX(multiPrimeNum) as lastPrime FROM multiPrimes;"
+            lastRange = multiSelect(sqlInput)
+            lastRangeInt = next((y for x in lastRange for y in x), None)
 
-    #add new range to be checked to inProgress table
-    if lastRangeInt == None:
-        lastRangeInt = 0
-    currentRangeStart = lastRangeInt + 1
-    currentRangeEnd = currentRangeStart + 100
-    userName = hostName()
-    sqlInput = "INSERT INTO inProgress (userID, numStartChecking, numEndChecking) VALUES ('" + str(userName[0]) + "', " + str(currentRangeStart) + ", " + str(currentRangeEnd) + ");"
-    multiUpdate(sqlInput)
+        if lastRangeInt is None:
+            lastRangeInt = 0
+        currentRangeStart = lastRangeInt + 1
+        currentRangeEnd = currentRangeStart + 100
 
-    #there can be cases where multiple program instances grab the same range to be checked at the same time. We need to ensure this does not happen.
-    #double check that no other instance has the same range as the one just grabbed.
+        # Insert new range into inProgress table
+        sqlInput = f"""
+            INSERT INTO inProgress (userID, numStartChecking, numEndChecking, inProgress)
+            VALUES ('{userName}', {currentRangeStart}, {currentRangeEnd}, 1);
+        """
+        multiUpdate(sqlInput)
+    
+    # Existing logic for handling multiple instances
     testCase = 1
     while testCase == 1:
-        #grab the last two ranges being checked
         sqlInput = "SELECT MAX(numStartChecking) as secondMax FROM inProgress WHERE numStartChecking NOT IN (SELECT Max(numStartChecking) FROM inProgress);"
         secondMaxRange = multiSelect(sqlInput)
-        for x in secondMaxRange:		#isRangeUnique is a list of tuples. iterates through to grab an int.
+        for x in secondMaxRange:
             for y in x:
                 secondMaxInt = y
         sqlInput = "SELECT Max(numStartChecking) as max FROM inProgress;"
         maxRange = multiSelect(sqlInput)
-        for x in maxRange:		#isRangeUnique is a list of tuples. iterates through to grab an int.
+        for x in maxRange:
             for y in x:
                 maxInt = y
 
         if secondMaxInt == 1:
-            #when database is first created, there is no second range to test against, so we skip this test entirely.
             isRangeUniqueInt = 0
             print("First range is being tested")
         elif secondMaxInt != 1:
-            #check if the max and secondMax overlap
             print("maxInt = " + str(maxInt))
             print("secondMaxInt = " + str(secondMaxInt))
             if maxInt <= (secondMaxInt + 100):
-                isRangeUniqueInt = 1    #ranges overlap, need a new range
+                isRangeUniqueInt = 1
                 print("test shows there are multiple ranges")
             elif maxInt > (secondMaxInt + 100):
-                isRangeUniqueInt = 0    #ranges do not overlap. all good.
+                isRangeUniqueInt = 0
                 print("test shows range is unique")
 
-        #if there are multiple instances of the last range being checked then do this
         if isRangeUniqueInt == 1: 
             print("adding the next range.")
             sqlInput = "SELECT MAX(multiPrimeNum) as lastPrime FROM multiPrimes;"
             lastRange = multiSelect(sqlInput)
-            for x in lastRange:		#lastPrime is a list of tuples. iterates through to grab an int.
+            for x in lastRange:
                 for y in x:
                     lastRangeInt = y
 
-            #add new range to be checked to inProgress table
-            if lastRangeInt == None:
+            if lastRangeInt is None:
                 lastRangeInt = 0
             currentRangeStart = lastRangeInt + 1
             currentRangeEnd = currentRangeStart + 100
@@ -208,11 +195,11 @@ def multiLoadRange():
             sqlInput = "INSERT INTO inProgress (userID, numStartChecking, numEndChecking) VALUES ('" + str(userName[0]) + "', " + str(currentRangeStart) + ", " + str(currentRangeEnd) + ");"
             multiUpdate(sqlInput)
 
-        #if there is only one unique case then break the loop and do nothing
         elif isRangeUniqueInt == 0:
             testCase = 0            
 
     return currentRangeStart
+
 
 #function to save found prime to the database
 # - multiPrimes: multiPrimeID (PRI, int, auto increment), primeIndex(int), multiPrimeNum (int)
@@ -229,18 +216,4 @@ def multiSavePrime(newTest):
 # main program start
 # ------------------
 
-#create task variables attached to functions
-calculatingTask = threading.Thread(target=calculating, name='calculatingTask')
-inputTask = threading.Thread(target=userInput, name='inputTask')
-
-#start a task for calculating, and a task to look for user input 'q' to quit the program
-print("Calculating task starting.\n")
-calculatingTask.start()
-print("Looking for user input task starting.\n")
-inputTask.start()
-
-#once both concurrent tasks end, we continue.
-calculatingTask.join()
-inputTask.join()
-
-print("all tasks complete.\n")
+calculating()
