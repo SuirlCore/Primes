ec85ac6003335705974bdbb9dd3e603292a5c3d5
SuirlCore <152679258+SuirlCore@users.noreply.github.com>
2025-03-01 10:35:49 -0700

    update

--- multiPrimesTest.py
+++ multiPrimesTest.py
@@ -39,7 +39,7 @@ def calculating():
                 multiSavePrime(newTest)
 
             #slow things down a bit.
-            time.sleep(0.01)
+            time.sleep(0.05)
 
         #edit the database to show the range has been completed
         sqlInput = "UPDATE inProgress SET inProgress = '0' WHERE userID = '" + userNameInput + "';"
