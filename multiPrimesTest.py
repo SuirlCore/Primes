2be845ad9743769c2eea9e542e42e74000243066
SuirlCore <152679258+SuirlCore@users.noreply.github.com>
2025-03-01 10:05:06 -0700

    update

--- multiPrimesTest.py
+++ multiPrimesTest.py
@@ -48,10 +48,10 @@ def calculating():
 
 
 #adjusts the sleep time dynamicaly to keep cpu load at 40%
-def adjust_sleep_time(target_cpu=40, decrease_step=0.0000000005, increase_step=0.0000000005, min_sleep=0.0000000005, max_sleep=1.0):
+def adjust_sleep_time(target_cpu=40, decrease_step=0.000000000000000005, increase_step=0.000000000000000005, min_sleep=0.000000000000000005, max_sleep=1.0):
 
     if not hasattr(adjust_sleep_time, "sleep_time"):
-        adjust_sleep_time.sleep_time = 0.0000001  # Start with 0.1 seconds
+        adjust_sleep_time.sleep_time = 0.000000000000001  # Start with 0.1 seconds
 
     cpu_usage = psutil.cpu_percent(interval=0.1)
     print(cpu_usage)
@@ -61,7 +61,7 @@ def adjust_sleep_time(target_cpu=40, decrease_step=0.0000000005, increase_step=0
     else:
         adjust_sleep_time.sleep_time = min(max_sleep, adjust_sleep_time.sleep_time + increase_step)
 
-    print(format(adjust_sleep_time.sleep_time, ".10f"))
+    print(format(adjust_sleep_time.sleep_time, ".20f"))
     return adjust_sleep_time.sleep_time
 
 
